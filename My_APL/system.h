05b97b2436d0149af3f7e735f601ab11c5befd99
Komeiji-Satori-1 <1610887087@qq.com>
2026-05-07 20:08:35 +0800

    删除了My_BLL等四个文件夹。全部整合为ad9833文件夹

--- My_APL/system.h
+++ My_APL/system.h
@@ -2,18 +2,7 @@
 #define system_H
 
 #include "main.h"
-//HDL
-
-#include "My_SPI.h"
-
-//FML
-#include "ad9833.h"
-//BLL
-#include "dds.h"
-//APL
-#include "dds_apl.h"
-
 #include "usart_deal.h"
-
+#include "ad9833.h"
 void System_Init(void);
 #endif // system_H
